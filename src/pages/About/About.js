750d0cb699061ed12997b9b2446896bb4103fddf
sasha-murray <sasha.murray@appliedtechnology.se>
2020-01-14 10:37:17 +0100

    Styled pages, and rewrote to semantic HTML

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -3,8 +3,29 @@ import React from 'react'
 
 export const About = () => {
   return (
-    <div className="App">
-      <h1>I also like philosophy and books and maths and stuff</h1>
+    <div className="container">
+      <article className="about">
+        <section>
+          <header className="header">
+            <h1>About Me</h1>
+            <h2>I also like philosophy and books and maths and stuff</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero, natus. Quo est cupiditate vitae impedit?</p>
+          </header>
+          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Optio culpa ad, nulla fuga expedita quia sed tempora ipsa laudantium voluptate rerum possimus accusantium perferendis eveniet ducimus repellendus exercitationem obcaecati sunt!</p>
+          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. At inventore quos ut temporibus assumenda consequatur esse quam dicta, quidem eius doloribus aliquid fugit culpa voluptate, ad enim nesciunt minima beatae vel, provident labore accusamus praesentium fuga accusantium! Reiciendis, quae nesciunt.</p>
+        </section>
+        <br/>
+        <section>
+          <header className="header">
+            <h1>About this website</h1>
+            <h2>Semantic HTML is the future</h2>
+          </header>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate mollitia provident cum ratione molestiae, officia ea. Ab vel quam consectetur quo expedita delectus et officiis exercitationem magnam dolores, odio molestias.</p>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit porro aspernatur dicta est at exercitationem aliquid consequatur, atque error, odio esse, quas doloremque consequuntur quo necessitatibus. Eveniet illum voluptatum architecto.</p>
+          <p>Beatae reiciendis mollitia tempore saepe, error quo laudantium odio quod, quibusdam nam voluptates! Unde, quasi laudantium. Laborum amet nihil eos illum debitis cumque animi magni modi labore alias, quod temporibus.</p>
+          <p>Ullam recusandae architecto in accusamus nobis unde, harum odit pariatur modi. Distinctio nisi ex, earum tempora eos molestiae ab harum accusantium alias sit quos vitae possimus eius assumenda temporibus in.</p>
+        </section>
+      </article>
     </div>
   )
 }
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -4,10 +4,18 @@ import me from '../../assets/me.jpeg'
 
 export const Home = () => {
   return (
-    <div className="App">
-      <h1>I'm Sasha Murray</h1>
-      <h2>I write code and stuff</h2>
-      <img src={me} className="portrait" alt="portrait of Sasha"/>
+    <div className="container">
+      <article className="bio">
+        <header className="header">
+          <h1>I'm Sasha Murray</h1>
+          <h2>I write code and stuff</h2>
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quisquam similique est ea facilis maxime deleniti placeat molestiae corporis eveniet eligendi!</p>
+        </header>
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tenetur dolorum fuga recusandae neque voluptates praesentium ea blanditiis earum delectus quod. Quos aut tempora qui quam minima. Similique at veritatis inventore excepturi consequuntur reiciendis voluptate molestiae perspiciatis totam magnam eos laborum minus amet, quas culpa cupiditate! Ex non illo quos labore!
+        </p>
+      </article>
+      <img src={me} className="portrait" alt="portrait of Sasha" />
     </div>
   )
 }
