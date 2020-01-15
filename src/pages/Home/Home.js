661e8123d7a98baff1b6abf1a7b2216164163aa8
sasha-murray <sasha.murray@appliedtechnology.se>
2020-01-15 14:29:59 +0100

    Content

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 
 export const Home = () => {
@@ -6,12 +7,13 @@ export const Home = () => {
     <div className="container">
       <article className="bio">
         <header className="header">
-          <h1>Hi, I'm Sasha Murray.</h1>
-          <h2>I'm a web developer and programmer based in Stockholm</h2>
-          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quisquam similique est ea facilis maxime deleniti placeat molestiae corporis eveniet eligendi!</p>
+          <h1>Hi, I'm Sasha Murray</h1>
+          <h2>I'm a versatile full-stack web developer and programmer.</h2>
+          <p>I have a diverse set of skills, ranging from design, semantic HTML, React and Angular on the front-end to Express, API, and databases on the back-end.  </p>
+          <p>For a more comprehenive overview of my skills and technologies, take a look <Link to='/about'>here</Link></p>
         </header>
         <p>
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tenetur dolorum fuga recusandae neque voluptates praesentium ea blanditiis earum delectus quod. Quos aut tempora qui quam minima. Similique at veritatis inventore excepturi consequuntur reiciendis voluptate molestiae perspiciatis totam magnam eos laborum minus amet, quas culpa cupiditate! Ex non illo quos labore!
+
         </p>
       </article>
     </div>
