4d4e8fb4de190204381d5ba561bf7107b8c4bcb2
sasha-murray <sasha.murray@appliedtechnology.se>
2020-01-16 13:56:50 +0100

    escaping apostrophes

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,14 +1,13 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-
 export const Home = () => {
   return (
     <div className="container">
       <article className="bio">
         <header className="header">
-          <h1>Hi, I'm Sasha Murray</h1>
-          <h2>I'm a versatile full-stack web developer and programmer.</h2>
+          <h1>Hi, I&#39;m Sasha Murray</h1>
+          <h2>I&#39;m a versatile full-stack web developer and programmer.</h2>
           <p>I have a diverse set of skills, ranging from design, semantic HTML, React and Angular on the front-end to Express, API, and databases on the back-end.  </p>
           <p>For a more comprehenive overview of my skills and technologies, take a look <Link to='/about'>here</Link></p>
         </header>
